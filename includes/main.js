0d924d409c79dc4fdc2d52166bc92581a3314e5d
Eric Lowry <ericmichaellowry@gmail.com>
2019-10-06 22:14:59 -0700

    Added JS to loop through array & create elements

--- includes/main.js
+++ includes/main.js
@@ -19,6 +19,7 @@ var pictures = [
 	'images/pretty.jpg',
 ];
 
+
 function initiateApp(){
 	/*advanced: add jquery sortable call here to make the gallery able to be sorted
 		//on change, rebuild the images array into the new order
@@ -30,19 +31,30 @@ function makeGallery(imageArray){
 	//use loops and jquery dom creation to make the html structure inside the #gallery section
 
 	//create a loop to go through the images in the imageArray
+	for(var i=0; i < imageArray.length; i++){
+		var period = imageArray[i].lastIndexOf(".");
+		var fileName = imageArray[i].substring(7, period);
+		var figure = $('<figure class="imageGallery col-xs-12 col-sm-6 col-md-4"></figure>').css('background-image', 'url(' + imageArray[i] + ')');
+		var figCaption = $('<figcaption>' + fileName + '</figcaption>');
+
+
+
+		var fullFigure = figure.append(figCaption);
+		$('#gallery').append(fullFigure);
+	}
 		//create the elements needed for each picture, store the elements in variable
 
-		//attach a click handler to the figure you create.  call the "displayImage" function.  
+		//attach a click handler to the figure you create.  call the "displayImage" function.
 
 		//append the element to the #gallery section
-	
+
 	// side note: make sure to remove the hard coded html in the index.html when you are done!
 
 }
 
 function addModalCloseHandler(){
 	//add a click handler to the img element in the image modal.  When the element is clicked, close the modal
-	//for more info, check here: https://www.w3schools.com/bootstrap/bootstrap_ref_js_modal.asp	
+	//for more info, check here: https://www.w3schools.com/bootstrap/bootstrap_ref_js_modal.asp
 }
 
 function displayImage(){
@@ -56,11 +68,6 @@ function displayImage(){
 	//change the modal-title text to the name you found above
 	//change the src of the image in the modal to the url of the image that was clicked on
 
-	//show the modal with JS.  Check for more info here: 
+	//show the modal with JS.  Check for more info here:
 	//https://www.w3schools.com/bootstrap/bootstrap_ref_js_modal.asp
 }
-
-
-
-
-
